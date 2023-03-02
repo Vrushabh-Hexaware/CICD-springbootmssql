35c4813a706cf99fc3280f8ea9a42a995dfd3a65
Vrushabh-Hexaware <1000075392@hexaware.com>
2023-03-02 11:19:58 +0000

    spingbootmssql push

---
+++ SpringBootwithMSSQL/src/main/java/com/SpringBootwithMSSQL/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.SpringBootwithMSSQL.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+	public EntityNotFoundException(String exception) {
+		super(exception);
+	}
+}
